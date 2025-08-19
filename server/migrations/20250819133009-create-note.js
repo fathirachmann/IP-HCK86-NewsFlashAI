36f2c4e1ab089c0de5ed0bdca991cd0f7e0701dd
fathirachmann <fathirachmann@gmail.com>
2025-08-19 20:55:25 +0700

    fix: note migration

--- server/migrations/20250819133009-create-note.js
+++ server/migrations/20250819133009-create-note.js
@@ -1,31 +1,27 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Notes', {
+    await queryInterface.createTable("Notes", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       articleId: {
-        type: Sequelize.INTEGER
-      },
-      content: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
+        type: Sequelize.INTEGER,
         allowNull: false,
-        type: Sequelize.DATE
+        references: { model: "Articles", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+      content: { type: Sequelize.TEXT, allowNull: false },
+      createdAt: { allowNull: false, type: Sequelize.DATE },
+      updatedAt: { allowNull: false, type: Sequelize.DATE },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Notes');
-  }
+    await queryInterface.dropTable("Notes");
+  },
 };
